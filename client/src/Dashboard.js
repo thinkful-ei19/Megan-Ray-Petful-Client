db1c68c52173585a701b632c2c3de8fdfa121f9a
leonardom249 <leonardo.megan@gmail.com>
2018-05-25 11:10:12 -0500

    separated into components

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Pet from './components/Pet'
+import './Dashboard.css';
+
+
 
 export default class Dashboard extends React.Component{
     constructor(){
@@ -6,16 +10,20 @@ export default class Dashboard extends React.Component{
     
         this.state={
             catToAdopt:{
+                class:'catToAdopt',
                 name:'Charlie',
                 age:'3mnths',
+                sex:'female',
                 breed: 'kitten',
                 story:'teststestet',
                 img:'https://lh3.googleusercontent.com/aGFfr04fxrySBqOnIMfW6ibioA7lJs-Mu1S_2aJ9ionBywFvM_gei2r9hOEWQaJCMkByezdwuR3m3gw1vc2-3s1hFlQZDrg-SfVPE_9rCiz9j9TvknVFipRFZaXDl5gtNquEF6d4=w2400'
             },
             dogToAdopt:{
+                class:'dogToAdopt',
                 name:'Doggo',
                 age:'3mnths',
-                breed: 'kitten',
+                sex: 'male',    
+                breed: 'doggy',
                 story:'teststestet',
                 img:'https://lh3.googleusercontent.com/FY6jhC1EVK7THmrsNwQdNOsVyKYaDEvwOIL9WWagB17iTekclKIQhJGJ08KcyzUpKnvrcR93v4e6CDAV42ewpcZd8ru9lr-60Hznl3g8MEyJpypTBj-3tjwxrcgcoWoWkZTud4jx=w2400'
             }
@@ -25,45 +33,8 @@ export default class Dashboard extends React.Component{
     render() {
         return (
           <div className="App">
-            <div className='catToAdopt'>
-                <header>
-                    <img alt='adoptable cat' src={this.state.catToAdopt.img}/>
-                    <h2>More About {this.state.catToAdopt.name}</h2>
-                </header>
-                <main>
-                    <dl>
-                        <dt>Sex</dt>
-                        <dd>{this.state.catToAdopt.sex}</dd>
-                        <dt>Age</dt>
-                        <dd>{this.state.catToAdopt.age}</dd>
-                        <dt>Breed</dt>
-                        <dd>{this.state.catToAdopt.breed}</dd>
-                        <dt>Story</dt>
-                        <dd>{this.state.catToAdopt.story}</dd>
-                    </dl>
-                </main>
-                <button type='submit'>Adopt</button>
-            </div>
-
-            <div className='dogToAdopt'>
-            <header>
-                    <img alt='adoptable dog' src=''/>
-                    <h2>More About {this.state.dogToAdopt.name}</h2>
-                </header>
-                <main>
-                    <dl>
-                        <dt>Sex</dt>
-                        <dd>{this.state.dogToAdopt.sex}</dd>
-                        <dt>Age</dt>
-                        <dd>{this.state.dogToAdopt.age}</dd>
-                        <dt>Breed</dt>
-                        <dd>{this.state.dogToAdopt.breed}</dd>
-                        <dt>Story</dt>
-                        <dd>{this.state.dogToAdopt.story}</dd>
-                    </dl>
-                </main>
-                <button type='submit'>Adopt</button>
-            </div>
+            <Pet pet={this.state.catToAdopt}/>
+            <Pet pet={this.state.dogToAdopt}/>
           </div>
         );
       }
---
+++ client/src/components/Pet.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Pet(props){
+    return(
+        <div className={props.pet.class}>
+                <header>
+                    <img alt={props.pet.name} src={props.pet.img}/>
+                    <h2>More About {props.pet.name}</h2>
+                </header>
+                <main>
+                    <dl>
+                        <dt>Sex</dt>
+                        <dd>{props.pet.sex}</dd>
+                        <dt>Age</dt>
+                        <dd>{props.pet.age}</dd>
+                        <dt>Breed</dt>
+                        <dd>{props.pet.breed}</dd>
+                        <dt>Story</dt>
+                        <dd>{props.pet.story}</dd>
+                    </dl> 
+                 </main>
+                <button type='submit'>Adopt</button>
+            </div>
+    )
+}
